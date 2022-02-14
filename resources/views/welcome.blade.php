ee996fcfa0d274d35a4058f41cfb53405a878077
masbarok193@gmail.com <masbarok193@gmail.com>
2022-02-14 12:20:40 +0700

    Kode Perubahan Ketiga

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0 ">
             <div class=" ml-4 text-lg leading-7 font-semibold ">
-                <h1 class="text-gray-900 dark:text-white ">Muhammad Rizki Mubarok xxx:'</h1>
+                <h1 class="text-gray-900 dark:text-white ">Muhammad Rizki Mubarok '</h1>
             </div>
         </div>
     </body>
